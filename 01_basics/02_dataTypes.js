f0cc7e17951295f91e6eedc49b9b07c7f1902e54
Abothacker <aryajobs2022@gmail.com>
2025-05-20 13:20:54 +0000

    dataTypes complete

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+
+// number => 2 to power 53
+// bigint
+// numbers 
+// string => "Abot"
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+let name = "Abot" // string
+console.log(typeof name);
+console.log(typeof(name));
+
+
+let age = 18
+
+console.log(typeof age);
+console.log(typeof(age));
+
+let isLoggedIn = false
+let state;
+
+
+console.log(isLoggedIn); 
+console.log(state);
+
+
