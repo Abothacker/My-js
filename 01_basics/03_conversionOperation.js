0712fa1d699f6676e70131d86fe72d8b7b6d3d5f
Abothacker <aryajobs2022@gmail.com>
2025-05-20 13:38:50 +0000

    ConversionOperation  complete

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+//  numbers to string
+let age = 18
+valueStr = String(age)
+
+// console.log(typeof valueStr);
+// console.log(valueStr);
+
+// simple opereations //
+
+// console.log(2+2);
+// console.log(10-5);
+// console.log(5*2);
+// console.log(2**3);
+// console.log(10/3);
+// console.log(5%2);
+
+// If both values are not the same data type, the operation is incorrect
+
+console.log(2+"abc");  
+console.log("abc"+5);
+console.log(5+2+"abc");  
+
