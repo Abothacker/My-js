22cce15322b8e1e2f35108cfcc98f5b8e09c6c8a
Abothacker <aryajobs2022@gmail.com>
2025-05-20 13:48:39 +0000

    04 compairsion complete

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log(null < 1);
+// console.log(undefined < 1);
+
+
+console.log("1" == 1);
+console.log("1" === 1); // also dataType comparistion  "str"  ans "number"
