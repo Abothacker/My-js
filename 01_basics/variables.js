44e719d86d415c9673646482a227da34f7eb9b5d
Abothacker <aryajobs2022@gmail.com>
2025-05-20 13:11:07 +0000

    Variables complete

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const id = 123
+
+// id = 321  const variables are NOT allowed
+
+
+let name = "abot"
+name = "Rocky" 
+
+
+/* 3. 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+var age = 18
+
+age = 20  
+console.table([id, name, age])
