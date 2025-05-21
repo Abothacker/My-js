f2bf5ab2374b85eaf67d5d7af0764bfbe07145e0
Abothacker <aryajobs2022@gmail.com>
2025-05-21 07:33:55 +0000

    Num and Maths

---
+++ 01_basics/07_num_and_maths.js
@@ -0,0 +1,43 @@
+let num = 100
+
+
+console.log(num);
+
+
+console.log(num.toString());  // number to string //after able to use all string method
+
+
+console.log(typeof num.toString());
+
+
+let numTwo = 100.123;
+
+console.log(numTwo.toFixed(2));  // Get 2 digits after the decimal
+
+
+console.log(numTwo.toPrecision(1)) 
+
+
+let numThree = 10000
+
+console.log(numThree);
+
+console.log(numThree.toLocaleString('en-IN'));
+
+
+// .......................Maths....................
+
+
+console.log(Math); // object
+
+console.log(Math.abs(-2)); // OP => 2
+
+console.log(Math.round(200.623)); //round fig
+
+
+
+
+
+
+
+
