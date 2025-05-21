2f6b60c206f015b98c86b98aca74740b67c29402
Abothacker <aryajobs2022@gmail.com>
2025-05-21 17:19:58 +0000

    Array 1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,50 @@
+ // //Array
+
+// let myArr = [1,2,3,4,5,6]
+// let heros = ["iron man","loki", "SuperMan"]
+// let myArr2 = new Array(1,2,3,3)
+
+// console.log(myArr);
+// console.log(heros);
+// console.log(myArr2);
+
+
+// console.log(typeof myArr);
+
+
+
+//------------- methods in array ------------//
+
+
+let newArr = ["abot","rock","harry"]
+
+// newArr.push("iron man") // Add new value at end of array
+// newArr.pop()  // remove last value
+
+// newArr.unshift(3)  // add 3 at strting of the array
+// newArr.shift()  // remove 3
+
+
+// console.log(newArr.includes("rock")); // check value is present in array OP=> true
+
+//------------ slice and splice  ---------------------//
+let mainArr = ["iron man", "loki", "thor", "hulk", "superman"]
+console.log("A",mainArr);
+
+let mainArr2 = mainArr.slice(1,3)
+console.log("B",mainArr2);
+
+let mainArr3 = mainArr.splice(1,3)
+console.log("C",mainArr3);
+
+
+console.log(mainArr); 
+
+
+
+
+
+
+
+
+
