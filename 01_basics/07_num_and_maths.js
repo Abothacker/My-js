66e182d3868f6dbcf9dc68f8a6a9dbde60194ef1
Abothacker <aryajobs2022@gmail.com>
2025-05-21 15:57:45 +0000

    Number and Maths

--- 01_basics/07_num_and_maths.js
+++ 01_basics/07_num_and_maths.js
@@ -1,43 +1,48 @@
-let num = 100
+// let num = 100
 
 
-console.log(num);
+// console.log(num);
 
 
-console.log(num.toString());  // number to string //after able to use all string method
+// console.log(num.toString());  // number to string //after able to use all string method
 
 
-console.log(typeof num.toString());
+// console.log(typeof num.toString());
 
 
-let numTwo = 100.123;
+// let numTwo = 100.123;
 
-console.log(numTwo.toFixed(2));  // Get 2 digits after the decimal
-
-
-console.log(numTwo.toPrecision(1)) 
+// console.log(numTwo.toFixed(2));  // Get 2 digits after the decimal
 
 
-let numThree = 10000
-
-console.log(numThree);
-
-console.log(numThree.toLocaleString('en-IN'));
+// console.log(numTwo.toPrecision(1)) 
 
 
-// .......................Maths....................
+// let numThree = 10000
+
+// console.log(numThree);
+
+// console.log(numThree.toLocaleString('en-IN'));
 
 
-console.log(Math); // object
-
-console.log(Math.abs(-2)); // OP => 2
-
-console.log(Math.round(200.623)); //round fig
+// // .......................Maths....................
 
 
+// console.log(Math); // object
+
+// console.log(Math.abs(-2)); // OP => 2
+
+// console.log(Math.round(200.623)); //round fig
 
 
 
+
+
+// console.log(Math.random());
+
+// console.log((Math.random() *10) +1);
+
+console.log(Math.round(Math.random() *10) +1);
 
 
 
