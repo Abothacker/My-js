09394f0eade301972691cb01958e1aec1fe32453
Abothacker <aryajobs2022@gmail.com>
2025-05-22 17:37:24 +0000

    Objects Complete

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,53 @@
+// singleton
+// Object.create
+let myObj = {}
+myObj.name = "arun"
+myObj.age = 17
+
+// console.log(myObj);
+
+let user = {
+    fullname : {
+        userName : {
+            name : "rock",
+            age : 50
+        }
+    }
+}
+
+//console.log(user);
+//console.log(user.fullname.userName.name);
+
+
+//  combine objects
+
+let obj1 ={ 1: "A", 2: "B"}
+let obj2 ={ 3: "A", 4: "B"}
+let obj3 ={ 5: "A", 6: "B"}
+
+
+// let allObj = {obj1, obj2, obj3}
+
+
+let allObj = {...obj1,...obj2,...obj3}
+//console.log(allObj);
+
+let allObj2 = Object.assign({},obj1,obj2,obj3)
+//console.log(allObj2);
+
+
+console.log(user);
+//console.log(user.fullname.userName.name);
+
+
+const course = {
+    coursename: "JavaScript",
+    price: "Free",
+    courseInstructor: "hitesh"
+}
+
+
+
+let {coursename: getN} = course
+
+console.log(getN);
