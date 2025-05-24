ed36310d30adafbcfc47c18f8b3037bf5df5fa7e
Abothacker <aryajobs2022@gmail.com>
2025-05-24 17:46:52 +0000

    Switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,16 @@
+const month = 1
+switch (month){
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("fab");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+}
