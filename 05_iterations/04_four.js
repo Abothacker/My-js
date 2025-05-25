1f31fac459f6ef31f59b347f1b4708ec5e16e3a0
Abothacker <aryajobs2022@gmail.com>
2025-05-25 18:31:31 +0000

    for in

---
+++ 05_iterations/04_four.js
@@ -0,0 +1,33 @@
+// for in
+
+let myObj ={
+    name : "aryan",
+    age : 18,
+    email : "a@aryan.com"
+}
+
+for (const id in myObj) {
+    // console.log(id);  // key
+   // console.log(myObj[id]);  // for value 
+
+}
+
+
+let arr = ['js', 'java', 'py', 'cpp']
+
+for(const a in arr){
+    // console.log(arr[a]);
+    
+}
+
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
