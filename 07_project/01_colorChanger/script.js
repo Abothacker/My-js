e1f389c72c27acc16f341e2d1c4f0e0ff8ec2308
Abothacker <aryajobs2022@gmail.com>
2025-05-31 13:19:21 +0000

    dom project one complete

---
+++ 07_project/01_colorChanger/script.js
@@ -0,0 +1,30 @@
+// console.log("hello aryan");
+
+let button = document.querySelectorAll(".button")
+console.log(button);
+const body =document.querySelector('body')
+
+
+button.forEach( function(btn){
+ btn.addEventListener('click', function(e){
+   if (e.target.id == "grey"){
+      body.style.backgroundColor = e.target.id
+      
+   }
+   if (e.target.id == "white"){
+      body.style.backgroundColor = e.target.id
+      
+   }
+   if (e.target.id == "blue"){
+      body.style.backgroundColor = e.target.id
+      
+   }
+   if (e.target.id == "yellow"){
+      body.style.backgroundColor = e.target.id
+      
+   }
+
+
+ })
+    
+})
